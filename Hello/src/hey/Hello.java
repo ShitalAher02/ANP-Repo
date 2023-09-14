4ea3e2bb6c81cdcbd6405c3b2e955c8ae3285783
shita <shita@Shital>
2023-09-14 10:59:38 +0530

    My First Commit.

--- Hello/src/hey/Hello.java
+++ Hello/src/hey/Hello.java
@@ -1,10 +1,10 @@
-package H;
+package hey;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello world");
 
 	}
 
